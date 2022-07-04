71119051f8bc4c11a5dcdad568037bb2586784b8
gylman <gylmanbm@gmail.com>
2022-07-04 17:21:25 +0900

    extract Gallery component

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+function Gallery() {
+  return (
+    <div className="gallery">
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-1.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-2.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-3.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-4.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-5.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-6.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-7.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-8.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-9.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-10.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-11.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+      <figure className="gallery-item">
+        <img
+          src="img/gallery/gallery-12.jpg"
+          alt="Beautifully
+arranged food"
+        />
+      </figure>
+    </div>
+  );
+}
+
+export default Gallery;
--- src/components/SectionTestimonials.jsx
+++ src/components/SectionTestimonials.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Testimonial from "./Testimonial";
 import cuid from "cuid";
+import Gallery from "./Gallery";
 
 const testimonialsArray = [
   {
@@ -57,93 +58,7 @@ function SectionTestimonials() {
           })}
         </div>
       </div>
-
-      <div className="gallery">
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-1.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-2.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-3.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-4.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-5.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-6.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-7.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-8.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-9.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-10.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-11.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-        <figure className="gallery-item">
-          <img
-            src="img/gallery/gallery-12.jpg"
-            alt="Beautifully
-    arranged food"
-          />
-        </figure>
-      </div>
+      <Gallery />
     </section>
   );
 }
