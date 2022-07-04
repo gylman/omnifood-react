77167d6e73868360d384dd18aa18ff8ff8463a4e
gylman <gylmanbm@gmail.com>
2022-07-04 15:59:20 +0900

    refactor SectionHow

--- src/components/SectionHow.jsx
+++ src/components/SectionHow.jsx
@@ -3,22 +3,38 @@ import StepTextBox from "./StepTextBox";
 
 const stepsArray = [
   {
-    stepNumber: "01",
+    stepNumber: 1,
+
     headingTertiary: "Tell us what you like (and what not)",
     stepDescription:
       "Never again waste time thinking about what to eat! Omnifood AI will create a 100% personalized weekly meal plan just for you. It makes sure you get all the nutrients and vitamins you need, no matter what diet you follow!",
+    img: {
+      src: "img/app/app-screen-1.png",
+      className: "step-img",
+      alt: "iPhone app preferences selection screen",
+    },
   },
   {
-    stepNumber: "02",
+    stepNumber: 2,
     headingTertiary: "Approve your weekly meal plan",
     stepDescription:
       "Once per week, approve the meal plan generated for you by Omnifood AI. You can change ingredients, swap entire meals, or even add your own recipes.",
+    img: {
+      src: "img/app/app-screen-2.png",
+      className: "step-img",
+      alt: "iPhone app preferences selection screen",
+    },
   },
   {
-    stepNumber: "03",
+    stepNumber: 3,
     headingTertiary: "Receive meals at convenient time",
     stepDescription:
       "Best chefs in town will cook your selected meal every day, and we will deliver it to your door whenever works best for you. You can change delivery schedule and address daily!",
+    img: {
+      src: "img/app/app-screen-3.png",
+      className: "step-img",
+      alt: "iPhone app preferences selection screen",
+    },
   },
 ];
 
@@ -31,54 +47,10 @@ function SectionHow() {
           Your daily dose of health in 3 simple steps
         </h2>
       </div>
-
-      {/* <!-- STEP 02 --> */}
       <div className="container grid grid--2-cols grid--center-v">
-        <StepTextBox
-          stepNumber={stepsArray[0].stepNumber}
-          headingTertiary={stepsArray[0].headingTertiary}
-          stepDescription={stepsArray[0].stepDescription}
-        />
-
-        <div className="step-img-box">
-          <img
-            src="img/app/app-screen-1.png"
-            className="step-img"
-            alt="iPhone app
-    preferences selection screen"
-          />
-        </div>
-
-        {/* <!-- STEP 02 --> */}
-        <div className="step-img-box">
-          <img
-            src="img/app/app-screen-2.png"
-            className="step-img"
-            alt="iPhone app
-    meal approving plan screen"
-          />
-        </div>
-        <StepTextBox
-          stepNumber={stepsArray[1].stepNumber}
-          headingTertiary={stepsArray[1].headingTertiary}
-          stepDescription={stepsArray[1].stepDescription}
-        />
-
-        {/* <!-- STEP 03 --> */}
-
-        <StepTextBox
-          stepNumber={stepsArray[2].stepNumber}
-          headingTertiary={stepsArray[2].headingTertiary}
-          stepDescription={stepsArray[2].stepDescription}
-        />
-        <div className="step-img-box">
-          <img
-            src="img/app/app-screen-3.png"
-            className="step-img"
-            alt="iPhone app
-    delivery screen"
-          />
-        </div>
+        {stepsArray.map(function(step) {
+          return <StepTextBox step={step} />;
+        })}
       </div>
     </section>
   );
--- src/components/StepTextBox.jsx
+++ src/components/StepTextBox.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 
-function StepTextBox(props) {
-  return (
-    <div className="step-text-box">
-      <p className="step-number">{props.stepNumber}</p>
-      <h3 className="heading-tertiary">{props.headingTertiary}</h3>
-      <p className="step-description">{props.stepDescription}</p>
-    </div>
+function StepTextBox({ step }) {
+  return step.stepNumber % 2 ? (
+    <>
+      <div className="step-img-box">
+        <img
+          src={step.img.src}
+          className={step.img.className}
+          alt={step.img.alt}
+        />
+      </div>
+      <div className="step-text-box">
+        <p className="step-number">
+          {step.stepNumber.toString().padStart(2, "0")}
+        </p>
+        <h3 className="heading-tertiary">{step.headingTertiary}</h3>
+        <p className="step-description">{step.stepDescription}</p>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="step-text-box">
+        <p className="step-number">
+          {step.stepNumber.toString().padStart(2, "0")}
+        </p>
+        <h3 className="heading-tertiary">{step.headingTertiary}</h3>
+        <p className="step-description">{step.stepDescription}</p>
+      </div>
+      <div className="step-img-box">
+        <img
+          src={step.img.src}
+          className={step.img.className}
+          alt={step.img.alt}
+        />
+      </div>
+    </>
   );
 }
 
